421c38972c3e84ee2831ca238f343ee49ee227e4
itilocao <italo.felipe.ignacio@hotmail.com>
2022-08-24 16:37:06 -0300

    Mudando position

--- src/pages/projects/pages/WebsitePatrimonies/website/components/Loading/styled.tsx
+++ src/pages/projects/pages/WebsitePatrimonies/website/components/Loading/styled.tsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import { primaryColor } from "../../config/colors";
 
 export const Container = styled.div`
-  position: absolute;
+  position: fixed;
   width: 100%;
   height: 100%;
   top: 0;
@@ -31,7 +31,7 @@ export const Container = styled.div`
   div {
     position: absolute;
     width: 100%;
-    height: 200%;
+    height: 100%;
     z-index: 1;
     background: rgba(0, 0, 0, 0.4);
   }
